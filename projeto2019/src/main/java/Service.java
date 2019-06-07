6af604c1e3eff12c76c1dc8910df5f3b1357fd16
Aluno <Aluno@DESKTOP-3FIS8IH>
2019-06-07 12:11:42 -0300

    07/06 ok

---
+++ projeto2019/src/main/java/Service.java
@@ -0,0 +1,15 @@
+import java.awt.List;
+
+public interface Service<E extends Identificavel> {
+
+	void save(E e);
+
+	void update(E e);
+
+	void remove(E e);
+
+	E getByID(long userId);
+
+	List getAll();
+
+}
--- projeto2019/src/main/java/UsuarioJaExisteException.java
+++ projeto2019/src/main/java/UsuarioJaExisteException.java
@@ -1,4 +1,5 @@
 
 public class UsuarioJaExisteException extends Exception {
+	
 
 }
